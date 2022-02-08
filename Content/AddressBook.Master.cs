d5b805e93b7b4bc71bc06de5ba3c4987d8e4dab9
Dhruvil Dobariya <190540107051@darshan.ac.in>
2022-02-08 23:54:24 +0530

    Add project files.

---
+++ Content/AddressBook.Master.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MultiUserAddressBook.Content
+{
+    public partial class AddressBook : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /*string path = HttpContext.Current.Request.Url.AbsolutePath;
+            string[] str = path.Split('/');
+
+            switch (str[2])
+            {
+                case "Country":
+                    hlCountry.CssClass = "nav-link active";
+                    break;
+                case "State":
+                    hlState.CssClass = "nav-link active";
+                    break;
+                case "City":
+                    hlCity.CssClass = "nav-link active";
+                    break;
+                case "ContactCategory":
+                    hlContactCategory.CssClass = "nav-link active";
+                    break;
+                case "Contact":
+                    hlContact.CssClass = "nav-link active";
+                    break;
+            }*/
+        }
+    }
+}
